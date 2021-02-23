e7445b05fee2bd593ae19a9f1147606a2abae99d
Sergio Marques <sergio@unicamp.br>
2021-02-22 21:46:17 -0300

    aula 220221

---
+++ 22-02-21/BasicoApp/app/src/main/java/br/com/slmm/basicoapp/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.slmm.basicoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*  alo mundo  */
+//ss
+
+//comentario do codigo
+/* Java Doc
+*
+*
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
